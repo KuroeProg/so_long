ad8ea0f7d643b80b58b814e4ce4a94d882432b4d
KuroeProg <itsumy81@gmail.com>
2024-12-19 21:28:57 +0100

    less lines, works better

--- window.c
+++ window.c
@@ -12,23 +12,12 @@ int main(void)
 
     mlx_connection = mlx_init();
     if (mlx_connection == NULL)
-    {
-        printf("mlx_init() failed\n");
-        return (1);
-    }
-
+        return (1)
     mlx_window = mlx_new_window(mlx_connection, WIDTH, HEIGHT, "So_long");
     if (mlx_window == NULL)
-    {
-        mlx_destroy_display(mlx_connection);
-        free(mlx_connection);
-        return (1);
-    }
-
+        return (mlx_destroy_display(mlx_connection), free(mlx_connection), 1);
     mlx_loop(mlx_connection);
-
     mlx_destroy_window(mlx_connection, mlx_window);
     mlx_destroy_display(mlx_connection);
     free(mlx_connection);
-
 }
